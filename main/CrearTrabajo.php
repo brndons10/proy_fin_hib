a6609d846c2e844403bd031a7f1d606675ff519d
Ana Martínez <32472338+anamartinezsan@users.noreply.github.com>
2020-06-03 23:00:13 -0600

    -sobreescritura

--- main/CrearTrabajo.php
+++ main/CrearTrabajo.php
@@ -1,46 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Creación</title>
-    </head>
-    <body>
-        <h2>Crear Trabajo</h2>
-        
-        <?php
-        // Validar si se obtienen los datos
-        $id= $_POST['id'];
-        $categoria= $_POST['categoria'];
-        $paga=$_POST['paga'];
-        $dir=$_POST['dir'];
-        $desc=$_POST['desc'];
-        $tel=$_POST['tel'];
-       include"CONEXION.php";
-        //Sentencia para insertar datos en la base 
-        $sentencia="insert into trabajos values ('$id','$categoria','$paga','$dir', '$desc', '$tel')";
-        echo"<br><br>";
-        
-        
-        //Ejecutar sentencia
-        $resultado= mysqli_query($conexion, $sentencia);
-        
-        //Validar registro agregado
-        $numero=  mysqli_affected_rows($conexion);
-        if($resultado && $numero==1){
-            echo "EL trabajo ".$categoria."se ha agregado exitosamente.";
-        }
-        else{
-            echo"No se ha podido agregar el trabajo ".$categoria;
-        }
-        echo"<a href='VerTrabajos.php'>  Volver </a>";
-        ?>
-    </body>
-</html>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -113,48 +71,5 @@ and open the template in the editor.
 </div>
 
 
-    </body>
-</html>
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Creación</title>
-    </head>
-    <body>
-        <h2>Crear Trabajo</h2>
-        
-        <?php
-        // Validar si se obtienen los datos
-        $id= $_POST['id'];
-        $categoria= $_POST['categoria'];
-        $paga=$_POST['paga'];
-        $dir=$_POST['dir'];
-        $desc=$_POST['desc'];
-        $tel=$_POST['tel'];
-       include"CONEXION.php";
-        //Sentencia para insertar datos en la base 
-        $sentencia="insert into trabajos values ('$id','$categoria','$paga','$dir', '$desc', '$tel')";
-        echo"<br><br>";
-        
-        
-        //Ejecutar sentencia
-        $resultado= mysqli_query($conexion, $sentencia);
-        
-        //Validar registro agregado
-        $numero=  mysqli_affected_rows($conexion);
-        if($resultado && $numero==1){
-            echo "EL trabajo ".$categoria."se ha agregado exitosamente.";
-        }
-        else{
-            echo"No se ha podido agregar el trabajo ".$categoria;
-        }
-        echo"<a href='VerTrabajos.php'>  Volver </a>";
-        ?>
     </body>
 </html>
